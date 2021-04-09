41dc8b68f47fb967f4c84dc1e8c5bd5a9155f204
Nils Müller <nils.biesalski@gmail.com>
2021-04-09 21:34:15 +0200

    :bug: Fix: typo

--- infrastructure/system/xinit/main.tf
+++ infrastructure/system/xinit/main.tf
@@ -2,15 +2,16 @@
 
 locals {
   xinitrc = file("${path.module}/xinitrc")
+  path = "/home/${var.user}"
 }
 
 resource "local_file" "xinitrc" {
   content = local.xinitrc
-  filename = "/home/${var.user}/.xinitrc"
+  filename = "${local.path}/.xinitrc"
   file_permission = "0600"
 }
 
-resource "null_resource" "package_install" {
+resource "null_resource" "ownership" {
   triggers = {
     xinitrc = sha1(local.xinitrc)
   }
@@ -19,6 +20,6 @@ resource "null_resource" "package_install" {
   ]
   provisioner "local-exec" {
     command = "chown ${var.user}: .xinitrc"
-    working_dir = "/home/${var.user}/"
+    working_dir = "${local.path}"
   }
 }
