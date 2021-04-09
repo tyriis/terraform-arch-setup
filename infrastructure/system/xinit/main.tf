7eceda7914be7bfcf2c65ee39d57855a3a9ed180
Nils Müller <nils.biesalski@gmail.com>
2021-04-09 21:10:13 +0200

    :bug: Fix: change file owner after creation for .xinitrc

--- infrastructure/system/xinit/main.tf
+++ infrastructure/system/xinit/main.tf
@@ -9,3 +9,16 @@ resource "local_file" "xinitrc" {
   filename = "/home/${var.user}/.xinitrc"
   file_permission = "0600"
 }
+
+resource "null_resource" "package_install" {
+  triggers = {
+    xinitrc = sha1(local.xinitrc)
+  }
+  depends_on = [
+    local_file.xinitrc
+  ]
+  provisioner "local-exec" {
+    command = "sudo chown ${var.user}: .xinitrc"
+    working_dir = "/home/${var.user}/"
+  }
+}
